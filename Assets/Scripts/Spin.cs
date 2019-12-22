d58bb722205ee52b1d0f2689365e3e236df5a2f5
AAALLLEEEXXX <aaallleeexxx918@gmail.com>
2019-12-22 12:34:28 +0300

    + Script; +Skybox

--- Assets/Scripts/Spin.cs
+++ Assets/Scripts/Spin.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Spin : MonoBehaviour
 {
+    public float speed = 3.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,6 @@ public class Spin : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(0, speed, 0);
     }
 }
