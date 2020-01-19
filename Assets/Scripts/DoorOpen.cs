4b86743b46b26f550ba527df34cc3ee995d40cc6
AAALLLEEEXXX <aaallleeexxx918@gmail.com>
2020-01-19 17:10:14 +0300

    +work Animation

---
+++ Assets/Scripts/DoorOpen.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpen : MonoBehaviour
+{
+    private bool _open;
+
+    public void Operate()
+    {
+        if (_open)
+        {
+            transform.Rotate(0, 90, 0);
+        }
+        else
+        {
+            transform.Rotate(0, -90, 0);
+        }
+        _open = !_open;
+    }
+    
+}
