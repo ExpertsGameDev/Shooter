1fc6f6585190b4084a3e5487958ca58e6bc9e0f5
AAALLLEEEXXX <aaallleeexxx918@gmail.com>
2020-01-28 12:42:04 +0300

    +AIEnemy

---
+++ Assets/Scripts/DoorTrigger.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTrigger : MonoBehaviour
+{
+    public GameObject[] targets;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            foreach(GameObject target in targets)
+            {
+                target.SendMessage("Open");
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            foreach (GameObject target in targets)
+            {
+                target.SendMessage("Close");
+            }
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
