1dab01b52b7e6e75c382bb144700fb191413c577
AAALLLEEEXXX <aaallleeexxx918@gmail.com>
2020-02-09 12:39:43 +0300

    + scene menu

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -7,19 +7,16 @@ public class SceneController : MonoBehaviour
     public GameObject enemyPrefab;
     private GameObject _enemy;
     private GameObject player;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
     void Update()
     {
         if(_enemy == null)
         {
             _enemy = Instantiate(enemyPrefab) as GameObject;
             _enemy.transform.position = gameObject.transform.position;
+            player = GameObject.FindWithTag("Player");//находим объект с тегом “Player”
+            _enemy.gameObject.GetComponent<AIEnemy>().target = player.transform;
+            _enemy.gameObject.GetComponent<AIEnemy>().player = player.GetComponent<PlayerCharacter>();
         }
     }
 }
