4167e57e3bf5f2081cf4a63d85ff5c2221389671
AAALLLEEEXXX <aaallleeexxx918@gmail.com>
2020-02-11 08:22:52 +0300

    + Menu

---
+++ Assets/Scripts/ButtonMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonMenu : MonoBehaviour
+{
+    public GameObject startMenu;
+    public GameObject settigsMenu;
+
+	public void ButtonStart()
+	{
+        SceneManager.LoadScene(1);
+	}
+
+    public void ButtonSetting()
+    {
+        startMenu.SetActive(false);
+        settigsMenu.SetActive(true);
+    }
+
+    public void ButtonExit()
+    {
+        Application.Quit();
+    }
+}
